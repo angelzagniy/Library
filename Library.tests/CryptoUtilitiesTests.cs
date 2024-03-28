661a1d73347ccf14ff78467c7c2ab2369fd05a16
Nick Nilga <myko@telenor.dk>
2024-03-28 12:52:12 +0200

    Add User model and crypto utilities

---
+++ Library.tests/CryptoUtilitiesTests.cs
@@ -0,0 +1,50 @@
+﻿using Library.Web.BusinessLogic.Security;
+using NUnit.Framework;
+
+namespace Library.Tests;
+
+[TestFixture]
+internal class CryptoUtilitiesTests
+{
+	[Test]
+	public void HashPassword_ShouldGenerateDifferentHashesForSamePasswords()
+	{
+		string password = Guid.NewGuid().ToString();
+		
+		(string HashedPassword, string Salt) resultOne = CryptoUtilities.HashPassword(password);
+		(string HashedPassword, string Salt) resultTwo = CryptoUtilities.HashPassword(password);
+		
+		Assert.That(resultOne.Salt, Is.Not.EqualTo(resultTwo.Salt).IgnoreCase);
+		Assert.That(resultOne.HashedPassword, Is.Not.EqualTo(resultTwo.HashedPassword).IgnoreCase);
+	}
+	
+	[Test]
+	public void IsPasswordValid_ShouldReturnTrueForValidPassword()
+	{
+		string password = Guid.NewGuid().ToString();
+		
+		(string HashedPassword, string Salt) newPassword = CryptoUtilities.HashPassword(password);
+		
+		bool isPasswordValid = CryptoUtilities.IsPasswordValid(
+			password, 
+			newPassword.HashedPassword,
+			newPassword.Salt);
+		
+		Assert.That(isPasswordValid, Is.True);
+	}
+	
+	[TestCase("qwerty", "Qwerty")]
+	[TestCase("1234", "123")]
+	public void IsPasswordValid_ShouldReturnFalseForInvalidPassword(string passwordOne, string passwordTwo)
+	{
+		(string HashedPassword, string Salt) originalPassword = CryptoUtilities.HashPassword(passwordOne);
+		
+		bool isPasswordValid = CryptoUtilities.IsPasswordValid(
+			passwordTwo, 
+			originalPassword.HashedPassword,
+			originalPassword.Salt);
+		
+		Assert.That(isPasswordValid, Is.False);
+	}
+
+}
---
+++ Library.web/Models/User.cs
@@ -0,0 +1,37 @@
+namespace Library.Web.Models;
+
+/// <summary>
+/// Model for library member.
+/// </summary>
+public class User
+{
+	/// <summary>
+	/// Member unique ID.
+	/// </summary>
+	public Guid Id { get; set; }
+
+	/// <summary>
+	/// User full name.
+	/// </summary>
+	public string Name { get; set; }
+
+	/// <summary>
+	/// User role.
+	/// </summary>
+	public string Role { get; set; }
+
+	/// <summary>
+	/// User login name.
+	/// </summary>
+	public string Username { get; set; }
+
+	/// <summary>
+	/// Hashed password.
+	/// </summary>
+	public string Password { get; set; }
+
+	/// <summary>
+	/// Password "salt".
+	/// </summary>
+	public string Salt { get; set; }
+}
